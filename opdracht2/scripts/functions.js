64afed60d21d43c03a74f0b4606e4a421774af28
WyroneBlue <wyroneblue@gmail.com>
2022-03-19 15:17:50 +0100

    smooth scroll to top

--- opdracht2/scripts/functions.js
+++ opdracht2/scripts/functions.js
@@ -19,8 +19,11 @@ const debounce = (callback, wait) => {
 }
 
 const goToTop = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0; 
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
 }
 
 export {
