e89c4a6c66d933231f69c47083302b615398032e
WyroneBlue <wyroneblue@gmail.com>
2022-04-06 17:38:34 +0200

    Create filter.js

---
+++ opdracht2/scripts/filter.js
@@ -0,0 +1,46 @@
+const filters = {
+    items: {
+        order: {
+            name: 'order',
+            items: 'A-Z',
+            get count(){
+                return 1
+            }
+        },
+        genre: {
+            name: 'genre',
+            items: [],
+            get count(){
+                return this.items.length
+            }
+        }
+    },
+    get count(){
+        return this.items.order.count + this.items.genre.count
+    }
+}
+
+const checkFiltersCount = () => {
+    return filters.count;
+}
+
+const updateFilter = (type, value) => {
+    console.log(filters.items[type]);
+    filters.items[type].items = value;
+}
+
+const checkFilter = (type, value) => {
+    return filters.items[type];
+}
+
+const resetFilters = (form) => {
+	form.reset();
+}
+
+export {
+	filters,
+    updateFilter,
+    checkFilter,
+    checkFiltersCount,
+	resetFilters
+}
